7f9f4cf84c74c0617414d347ef5cdd68e67bc790
Juho Syrjänen <syrjanen.juho@gmail.com>
2019-08-19 13:21:59 +0300

    Create app.php

---
+++ app.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>Testing PHP in App Engine</title>
+ </head>
+ <body>
+ <?php echo '<p>Hi, this is a scaling PHP web page, running on Google Cloud App Engine.</p>';
+       echo '<p> Yeehaw. </p>'; ?>
+ </body>
+</html>
